e20ab9ebd4ae453b6ed430c2b32359bd0b67ce86
Moealsir <mohamedwdalsir@gmail.com>
2024-06-08 02:26:11 +0300

    linked through get started button

--- app/lnading/hero.tsx
+++ app/lnading/hero.tsx
@@ -1,22 +1,15 @@
 import Link from "next/link";
-import React, { useState } from "react";
-import { useRouter } from "next/router";
+import React from "react";
 
 export default function Hero() {
-  // const [place, setPlace] = useState("");
-  // const router = useRouter();
 
-  // const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-  //   router.push(`/result/${place}`);
-  // };
 
   return (
     <div>
       <div className="px-4 py-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen md:px-24 lg:px-8 lg:py-0">
         <div className="max-w-2xl mx-auto sm:max-w-xl md:max-w-2xl">
           <div className="text-center">
-            <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-5">
+            <div className="max-w-xl mb-0 md:mx-auto sm:text-center lg:max-w-2xl md:mb-3">
               <div></div>
               <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
                 Stay Updated with the next week Weather
@@ -24,22 +17,20 @@ export default function Hero() {
               <p className="text-base text-gray-700 md:text-lg">
                 Accurate Weather Forecasts
               </p>
+              <div className="mt-4 flex gap-4"></div>
             </div>
-            <form className="flex flex-col items-center w-full mb-4 md:flex-row md:px-16">
-              <input
-                placeholder="Place"
-                // required=""
-                type="text"
-                className="flex-grow w-full h-12 px-4 mb-3 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none md:mr-2 md:mb-0 focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
-              />
-              <button
-                type="submit"
-                className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide bg-blue-500 text-white transition duration-200 rounded shadow-md md:w-auto bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-              >
-                Search
-              </button>
-            </form>
-            <p className="max-w-md mx-auto mb-4 text-xs text-gray-600 sm:text-sm md:mb-2">
+            <a
+              className="inline-flex justify-center rounded-lg text-sm font-semibold py-3 px-4 bg-slate-900 text-white hover:bg-slate-700"
+              href="/get_started"
+            >
+              <span>
+                Get Started{" "}
+                <span aria-hidden="true" className="text-slate-400 sm:inline">
+                  →
+                </span>
+              </span>
+            </a>
+            <p className="max-w-md mx-auto mb-4 mt-6 text-xs text-gray-600 sm:text-sm md:mb-2">
               Get the most precise and up-to-date weather information for your
               location. Stay prepared for any weather conditions with
               WeatherWave.
